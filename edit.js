d958dfda3c00c14c2d42a55137bad94687b72740
tazMeah <montazmeahii@yahoo.com>
2018-08-16 23:01:19 -0400

    update profile button logs the inputs to the console

--- edit.js
+++ edit.js
@@ -2,34 +2,33 @@
 
 const editProfile = {
   template: `
-    <figure>
-        
-        <figcaption>
-            <h2>Use the form below to update your profile.</h2>
-            <form>
-                <label>
-                    <h3>Name</h3>
-                    <input>
-                </label>
-                <label>
-                    <h3>Contact</h3>
-                    <input>
-                </label>
-                <label>
-                    <h3>Bio</h3>
-                    <input>
-                </label>
-            </form>
-            <span id="contact">grant@grandcircus.co</span>
-            <p id="bio">I am pretty cool. I a great job. I like to play Tetris. I am pretty ok at AngularJS.</p>
-        </figcaption>
+  
+    <form id="editForm">
+        <h2>Use the form below to update your profile.</h2>
+        <label>
+            <h3>Name</h3>
+            <input ng-model="$ctrl.inputs.name">
+        </label>
+        <label>
+            <h3>Contact</h3>
+            <input ng-model="$ctrl.inputs.contact">
+        </label>
+        <label>
+            <h3>Bio</h3>
+            <input ng-model="$ctrl.inputs.bio">
+        </label>
         <a>
-            <button>Update</button>
+            <button id="update" ng-click="$ctrl.sendToService($ctrl.inputs)">Update</button>
         </a>
-    </figure>
+    </form>
+            
+        
     `,
     controller: function () {
-        
+        const vm = this;
+        vm.sendToService = function(profileInputs) {
+            console.log(profileInputs)
+        }
     }
 
 };
