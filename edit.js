850c0ccb70388b9569318795a77047a0c3943ea5
tazMeah <montazmeahii@yahoo.com>
2018-08-19 10:40:54 -0400

    added functionality to preserve contents of profile screen in edit form

--- edit.js
+++ edit.js
@@ -7,16 +7,16 @@ const editProfile = {
         <h2>Use the form below to update your profile.</h2>
         <label>
             <h3>Name</h3>
-            <input ng-model="$ctrl.inputs.name" id="name">
+            <input ng-model="$ctrl.inputs.name" id="name" ng-value="$ctrl.inputs.name">
         </label>
         <label>
             <h3>Contact</h3>
-            <input ng-model="$ctrl.inputs.contact" id="contact">
+            <input ng-model="$ctrl.inputs.contact" id="contact" ng-value="$ctrl.inputs.contact">
         </label>
         <label>
             <h3>Bio</h3>
             <!--<input ng-model="$ctrl.inputs.bio" id="bio">-->
-             <textarea ng-model="$ctrl.inputs.bio" id="bio"></textarea>
+             <textarea ng-model="$ctrl.inputs.bio" id="bio" ng-value="$ctrl.inputs.bio"></textarea>
         </label>
         <a href="#!/profile">
             <button id="update" ng-click="$ctrl.sendToService($ctrl.inputs)">Update</button>
@@ -29,9 +29,8 @@ const editProfile = {
         const vm = this;
         vm.sendToService = function(profileInputs) {
             ProfileService.setUserProfile(profileInputs);
-            
-
         }
+        vm.inputs = ProfileService.getUserProfile();
     }]
 
 };
